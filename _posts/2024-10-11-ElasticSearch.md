33f9cf36b466c037dcf7030194c523d6fbb9b2e7
wang1 <1790362502@qq.com>
2024-10-11 15:02:18 +0800

    blog2024-10

---
+++ _posts/2024-10-11-ElasticSearch.md
@@ -0,0 +1,9 @@
+---
+layout: post 
+title: "ElasticSearch"
+author: "wang"
+date: 2024-10-11 14:57:00 +0800
+categories: [LLM]
+tags: [llm, ElasticSearch]
+---
+# ElasticSearch
--- _posts/MDCreating.bat
+++ _posts/MDCreating.bat
@@ -12,29 +12,33 @@ set month=%datetime:~4,2%
 set day=%datetime:~6,2%
 set hour=%datetime:~8,2%
 set minute=%datetime:~10,2%
-set second=00 
+set second=00
 
 :: Combine date and time into the Jekyll format
-set datetime=%year%-%month%-%day% %hour%:%minute%:%second% +0800
+set current_date=%year%-%month%-%day% %hour%:%minute%:%second% +0800
 
-:: Ask for the filename
-set /p filename=Please enter the file name: %year%-%month%-%day%-
+:: Ask for the file name suffix
+set /p userinput=Please enter the file name suffix:
 
-:: Create the Markdown file with Jekyll-compliant YAML front matter
-echo --- > "%filename%.md"
-echo layout: post >> "%filename%.md"
-echo title: "New Document" >> "%filename%.md"
-echo author: "wang" >> "%filename%.md"
-echo date: %datetime% >> "%filename%.md"
-echo categories: [blog] >> "%filename%.md"
-echo tags: [blog] >> "%filename%.md"
-echo --- >> "%filename%.md"
-echo # First line >> "%filename%.md"
+:: Construct the full file name as year-month-day-userinput.md
+set filename=%year%-%month%-%day%-%userinput%.md
+
+:: Create a new file based on the template
+(
+    echo --- 
+    echo layout: post 
+    echo title: "%userinput%"
+    echo author: "wang"
+    echo date: %current_date%
+    echo categories: [blog]
+    echo tags: []
+    echo ---
+) > "%filename%"
 
 :: Inform user that the file was created
-echo File "%filename%.md" has been created
+echo File "%filename%" has been created with the current date: %current_date%
 
 :: Open the file with Typora
-start typora "%filename%.md"
+start typora "%filename%"
 
 endlocal
